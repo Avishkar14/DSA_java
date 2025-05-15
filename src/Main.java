9d6cd9798561f2c2668b743e9060721ac05b7e3b
Avishkar S <avi.k.shelke@gmail.com>
2025-05-15 17:21:57 +0530

    test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.printf("enter your name : ");
+//        Scanner in=new Scanner(System.in) ;
+//        System.out.println("Hello "+in.nextLine() + " !");
+
+    }
+}
---
+++ src/Odd_even.java
@@ -0,0 +1,5 @@
+public class Odd_even {
+    public static void main(String[] args) {
+        
+    }
+}
