b49223b2f42ed923ba92a4515aa67b031e920864
Avishkar S <avi.k.shelke@gmail.com>
2025-08-06 00:10:33 +0530

    solutions.

---
+++ Questions/src/Solution.java
@@ -0,0 +1,31 @@
+<<<<<<< Updated upstream
+=======
+import java.util.Scanner;
+
+>>>>>>> Stashed changes
+class Solution {
+    public static int subtractProductAndSum(int n) {
+        int sum=0;
+        int prod=1;
+        while(n>0){
+
+        int rem=n%10;
+        int div=n/10;
+        n=div;
+        sum=sum+rem;
+        prod=prod*rem;
+
+        }
+        int result = prod - sum;
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=in.nextInt();
+        subtractProductAndSum(num);
+        System.out.println("Result :"+num);
+    }
+}
