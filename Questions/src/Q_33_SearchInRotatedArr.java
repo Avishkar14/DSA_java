259461302385c917f295689d0043a9ca3b97c7f0
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2025-08-17 22:17:23 +0530

    Rotated array completed Q

--- Questions/src/Q_33_SearchInRotatedArr.java
+++ Questions/src/Q_33_SearchInRotatedArr.java
@@ -2,66 +2,66 @@
 //needs to be remade as fails test case: [1,3] ,target = 3 and o/p = 1 but our ans is -1;
 public class Q_33_SearchInRotatedArr  {
     public static void main(String[] args) {
-        int[] nums = { 5, 6, 7, 0, 1, 2, 3, 4 };
-        int target = 3;
+        int[] nums = { 1 , 3 };
+        int target = 1;
         System.out.println(search(nums , target));
     }
     public static int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
         int max = max(nums);
-        int ans = 0;
-        while ( ans == 0 ){
-        if( target <  nums[end] ){
+            if( max == -1) {
+                //means array is normal sorted array
+               return  binarysearch( nums , target , start,  end );
+            }
+
+            if( target == nums[max]){
+                return max;
+            }
+
+        if( target <=  nums[end] ){
             //search on right
-            ans = binarysearch( nums , target , (max+1) ,  end );
+            return binarysearch( nums , target , (max+1) ,  end );
             // start = max + 1;
         }
-        else if( target > nums[end]){
+        else {
             //search of left
-           ans =  binarysearch( nums , target , start ,  (max - 1) );
+           return binarysearch( nums , target , start ,  (max - 1) );
             // end = max - 1;
         }
-        else {
-            //target = max
-            return max;
-        }
-        return ans;
-        }
-//        System.out.println(ans);
-        return -1;
     }
 
     public static int max( int[] nums ){
 
         int start = 0;
         int end = nums.length - 1;
-        // int max = 0;
+        // int max = 0; max is our pivot
 
-        while( start<=end ){
+        while( start <= end ){
 
             int mid = start + (end - start)/2;
-            if( nums[start] > nums[end] && nums[start] > nums[mid]){
+
+            if( mid < end && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            if ( start < mid && nums[mid] < nums[mid-1]){
+                return mid-1;
+            }
+
+            if(  nums[start] >= nums[mid]){
                 //search for max on left
-                end = mid ;
+                end = mid - 1;
 
             }
-            else if( nums[start] > nums[end] && nums[start] < nums[mid] ){
-                if( nums[mid+1] > nums[mid]) {
+            else {
                     //search for mid on right
-                    start = mid;
-                }
-                else{
-                    return mid;
-                }
-            }
-            else{
-                return mid;
+                    start = mid + 1;
             }
         }
         return -1;
 
     }
+
     public static int binarysearch( int[] nums , int target , int start , int end){
 
 
