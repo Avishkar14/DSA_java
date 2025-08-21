7e00a14bb185c30f94bf90914b47b8fa7be55297
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2025-08-21 21:39:59 +0530

    Q.1295_Find Numbers with Even Number of Digits
    Array ez Q

---
+++ Questions/src/Q_1295_FindNumwithEven.java
@@ -0,0 +1,26 @@
+public class Q_1295_FindNumwithEven {
+    public static void main(String[] args) {
+        int[] nums = { 555,901,482,1771 };
+        System.out.println(findNumbers(nums));
+    }
+
+        public static int findNumbers(int[] nums) {
+
+            int even=0;
+            for(int i=0; i < nums.length ; i++){
+                int count = 0;
+                while( nums[i] > 9){
+                    nums[i] = nums[i]/10;
+                    count++;
+                }
+                if ( nums[i] < 10 && nums[i] > 0 ){
+                    count++;
+                }
+                if( count%2 == 0){
+                    even++;
+                }
+            }
+            return even;
+        }
+
+}
