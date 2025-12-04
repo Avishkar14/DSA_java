909c6aea237305e0b6cef8fade1b2f4be5ac9611
Avishkar S <avi.k.shelke@gmail.com>
2025-12-04 22:12:38 +0530

    Accepted solution of reshape matrix. Can Improve Space complexity.

--- Questions/src/Q_566_ReshapeMat.java
+++ Questions/src/Q_566_ReshapeMat.java
@@ -4,10 +4,10 @@ class Q_566_ReshapeMat {
     public static void main(String[] args) {
 //        int[][] mat = {{ 0, 1},
 //                       { 2, 3},
-//                {4, 5}};
+//                        {4, 5}};  // use ip: (mat , 2, 3)
         int[][] mat = {{ 0, 1},
-                       { 2, 3}};
-        System.out.println(Arrays.deepToString(matrixReshape( mat , 4, 1)));
+                       { 2, 3}};    // use ip: (mat , 1, 4)
+        System.out.println(Arrays.deepToString(matrixReshape( mat , 1, 4)));
 
     }
     public static int[][] matrixReshape(int[][] mat, int r, int c) {
@@ -15,13 +15,17 @@ class Q_566_ReshapeMat {
 
         if( mat.length*mat[0].length == newMat.length*newMat[0].length &&
             mat.length != newMat.length ){
-                
+                int count = 0;
                 for(int row = 0; row < mat.length; row++){
                     for(int col = 0; col < mat[0].length; col++){
-                        int newCol = ((mat[0].length - 1)*row + row + col);
-                        int newRow = newCol / (newMat[0].length + 1);
-
+//                        int newCol = ((mat[0].length - 1)*row + row + col);
+//                        int newRow = newCol / (newMat[0].length + 1);
+//
+//                        newMat[newRow][newCol] = mat[row][col];
+                        int newCol = count%c;
+                        int newRow = count/c;
                         newMat[newRow][newCol] = mat[row][col];
+                        count++;
                     }
                 }
 
