aa53e2bbef11077ba8bcbfd64d2dc185f8e1e1f4
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2025-12-10 22:09:22 +0530

    Create Q_217_ContnDupli.java
    Ez if duplicate then return true Q.
    Used Arrays.sort() ; so next time better logic and much better tc needed ( curr tc = 20%tile ).

---
+++ Questions/src/Q_217_ContnDupli.java
@@ -0,0 +1,27 @@
+class Q_217_ContnDupli {
+    //psvm
+    public static boolean containsDuplicate(int[] nums) {
+        // if(nums.length > 10000){
+        //     return false;
+        // }
+        Arrays.sort(nums);
+        // for( int i = 0; i < nums.length - 1; i++){
+        //     // if( i < nums.length - 1)
+        //     for( int j = i + 1; j < nums.length; j++){
+        //         if( nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        int i = 0;
+        while( i < nums.length - 1){
+            if( nums[i] == nums[i + 1]){
+                return true;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
