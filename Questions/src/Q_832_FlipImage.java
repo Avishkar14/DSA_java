8c2c373649e5e06f7c40ea2973bb2b649ffb5cf1
Avishkar S <avi.k.shelke@gmail.com>
2025-12-23 21:25:53 +0530

    Much better TC and also simpler logic: But SC needs improvement

--- Questions/src/Q_832_FlipImage.java
+++ Questions/src/Q_832_FlipImage.java
@@ -6,7 +6,7 @@ class Q_832_FlipImage {
                 { 1, 1, 0},
                 { 1, 0, 1},
                 { 0, 0, 0} };
-        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
+        System.out.println(Arrays.deepToString(flipAndInvertImage2(image)));
 //        for(int i = 0; i < image.length; i++){
 //            System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
 //        }
@@ -28,6 +28,19 @@ class Q_832_FlipImage {
                 int temp = image[i][j];
                 image[i][j] = image[i][image.length - 1 -j];
                 image[i][image.length -1 - j] = temp;
+            }
+        }
+        return image;
+    }
+    //Much better TC and also simpler logic: But SC needs improvement
+    public static int[][] flipAndInvertImage2(int[][] image){
+        for(int i =0; i < image.length; i++){
+            for(int j = 0; j < (image[0].length+1)/2; j++ ){
+                //swap to make it reverse;
+                int temp = image[i][j];
+                (image[i][j] ) = ~image[i][image[0].length - j - 1]& 1;
+                image[i][image[0].length -j -1] = ~temp&1;
+
             }
         }
         return image;
