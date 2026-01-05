1a77f3b9fe5929701263e317c9ad812fc979d0f2
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-01-05 23:53:29 +0530

    Create Q_191_NoOf1Bits.java

---
+++ Questions/src/Q_191_NoOf1Bits.java
@@ -0,0 +1,14 @@
+class Q_191_NoOf1Bits {
+    //psvm
+    public static int hammingWeight(int n) {
+        int count = 0;
+
+        while( n > 0){
+            if( (n & 1) == 1){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+}
