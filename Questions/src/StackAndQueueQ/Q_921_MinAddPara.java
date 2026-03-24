01351515868b23a2c0eef4a8bcdf65e0fc0991de
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-03-24 21:35:11 +0530

    Ez stack implementation Q (Med LC Q)

---
+++ Questions/src/StackAndQueueQ/Q_921_MinAddPara.java
@@ -0,0 +1,26 @@
+class Q_921_MinAddPara {
+    public static int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        // int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            }
+            else{ 
+                // ')'
+                if( !stack.isEmpty() ){
+                    if( stack.peek() == '('){
+                        stack.pop();
+                    }
+                    else{
+                        stack.push(')');
+                    }
+                }
+                else{ //empty
+                    stack.push(')');
+                }
+            }
+        }
+        return stack.size();
+    }
+} 
