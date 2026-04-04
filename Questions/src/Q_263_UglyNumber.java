8a32565f185360dc7df4bed6601975a9c8e34309
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-04-04 08:56:49 +0530

    Easy Maths Implementation Q

---
+++ Questions/src/Q_263_UglyNumber.java
@@ -0,0 +1,22 @@
+class Q_263_UglyNumber {
+    public static boolean isUgly(int n) {
+        if(n < 1){
+            return false;
+        }
+        while( (n > 0) && ( (n%2 == 0) || (n%3 == 0) || (n%5 == 0) )  ){
+            if( n%2 == 0 ){
+                n /= 2;
+            }
+            else if( n%3 == 0 ){
+                n /= 3;
+            }
+            else if( n%5 == 0){
+                n /= 5;
+            }
+        }
+        if( n == 1 ){
+            return true;
+        }
+        return false;
+    }
+}
