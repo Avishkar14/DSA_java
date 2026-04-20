9b6d70a811912ac35dbbdbf354fe5d80e2b40aea
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-04-20 22:36:52 +0530

    Can improve logic - Simple 2D array based Q.
    Very bad TC and SC , can improve by using boolean[] instead of AL.

---
+++ Questions/src/Q_73_SetMatrixZeroes.java
@@ -0,0 +1,35 @@
+class Q_73_SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+        List<List<Integer>> zeroIndexes = new ArrayList<>();
+
+        //traverse and find indexes
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if( matrix[i][j] == 0){
+                    List<Integer> indexes = new ArrayList<>();
+                    indexes.add(i);
+                    indexes.add(j);
+                    zeroIndexes.add(indexes);
+                }
+            }
+        }
+
+        //Now make i and j 's as 0's;
+        for (int i = 0; i < zeroIndexes.size(); i++) {
+            List<Integer> indexes = zeroIndexes.get(i);
+            
+            int row = indexes.get(0);
+            for(int j = 0; j < matrix[0].length; j++){
+                matrix[row][j] = 0;
+            }
+
+            int col = indexes.get(1);
+            for(int j = 0; j < matrix.length; j++){
+                matrix[j][col] = 0;
+            }
+        }
+
+        return;
+
+    }
+}
