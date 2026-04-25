cf8bfd49e34f0dc2ade5a50e16f2d0e3de410ce2
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-04-25 22:13:46 +0530

    Medium Q : Implement addTwoNumbers for linked lists
    Should try once again by myself

---
+++ Questions/src/StackAndQueueQ/Q_AddTwoNum.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Q_AddTwoNum {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode node = new ListNode(0);
+        ListNode head = node;
+        int ans = 0, carry = 0;
+        while( l1 != null && l2 != null){
+            ans = l1.val + l2.val + carry;
+            carry = 0; //reset carry after use;
+            if( ans > 9){
+                node.val = ans%10;
+                carry = 1;
+            }
+            else{
+                node.val = ans;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+            if( l1 != null || l2 != null || carry == 1){
+                node.next = new ListNode(0);
+                node = node.next;
+            }
+            
+
+        }
+
+        //any one loop ended:
+        if( l1 == null){
+            //l2 remain :
+            while( l2 != null){
+                ans = l2.val + carry;
+                carry = 0;
+                if( ans > 9 ){
+                    node.val = ans%10;
+                    carry = 1;
+                }
+                else{
+                    node.val = ans;
+                }
+                l2 = l2.next;
+                if( l1 != null || l2 != null || carry == 1){
+                    node.next = new ListNode(0);
+                    node = node.next;
+                }                
+            }
+        }
+        else{
+            //l1 remain:
+            while( l1 != null){
+                ans = l1.val + carry;
+                carry = 0;
+                if( ans > 9 ){
+                    node.val = ans%10;
+                    carry = 1;
+                }
+                else{
+                    node.val = ans;
+                }
+                l1 = l1.next;
+                if( l1 != null || l2 != null || carry == 1){
+                    node.next = new ListNode(0);
+                    node = node.next;
+                }
+            }
+
+        }
+//carry remains:
+        if( carry == 1){
+            node.val = carry;
+        }
+        return head;
+    }
+}
