e5c99c90d7e29dc946ca89806e687fba0340690c
Avishkar K S <146617914+Avishkar14@users.noreply.github.com>
2026-04-27 21:14:31 +0530

    Easy to continue streak.

---
+++ Questions/src/Q_3842_ToggleLightBulb.java
@@ -0,0 +1,17 @@
+Q_3842_ToggleLightBulb Solution {
+    public static List<Integer> toggleLightBulbs(List<Integer> bulbs) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < bulbs.size(); i++){
+            if( ans.contains(bulbs.get(i)) ){
+                //remove:
+                ans.remove(bulbs.get(i));
+            }
+            else{
+                //add:
+                ans.add(bulbs.get(i));
+            }
+        }
+        ans.sort(null);
+        return ans;
+    }
+}
